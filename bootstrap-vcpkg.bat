e4ddda91a5f21ece383df12991de55ab8a79ede6
marijaT13 <tashevska254@gmail.com>
2021-10-03 12:55:06 +0200

    Update bootstrap-vcpkg.bat

--- bootstrap-vcpkg.bat
+++ bootstrap-vcpkg.bat
@@ -1,2 +1,3 @@
 @echo off
 powershell.exe -NoProfile -ExecutionPolicy Bypass "& {& '%~dp0scripts\bootstrap.ps1' %*}"
+cmake -B [build directory] -S . -DCMAKE_TOOLCHAIN_FILE=[path to vcpkg]/scripts/buildsystems/vcpkg.cmake
